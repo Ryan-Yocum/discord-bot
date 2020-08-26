e661d8b649d41893ef19f19d8572e6f4223ba139
Ryan Yocum <38407240+rtyocum@users.noreply.github.com>
2020-08-26 17:53:44 -0400

    added async

--- cmds.js
+++ cmds.js
@@ -1,31 +1,9 @@
 // This file handes all the commands
 module.exports = {
-	cmds: function(dc, embed, msg, message, config) {
+	cmds: function (dc, embed, msg, message, config) {
 		switch (msg) {
-		case 'ping':
-			message.channel.send('pong');
-			break;
-
-		case 'hello':
-			message.channel.send('bye', {
-				reply: message.author,
-			});
-			break;
-
-		case 'embed':
-			embed.setAuthor(message.author.tag, message.author.defaultAvatarURL);
-			embed.setTitle('Embedded');
-			message.channel.send(embed);
-			break;
-
-		case 'pi':
-			message.channel.send('3.141592653589', {
-				reply: dc.users.cache.get('681600426973593611'),
-			});
-			break;
-
-		default:
-			break;
+			default:
+				break;
 		}
 	},
 };
--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 // Includes of the libraries
 const Discord = require('discord.js');
 require('dotenv').config();
-const sqlite3 = require('sqlite3');
-const token = process.env.TOKEN;
 const config = require('./config.json');
 const cmds = require('./cmds.js');
 
-
 // Create a Discord Client
 const dc = new Discord.Client();
+// const announce = new Discord.WebhookClient(process.env.HOOK, process.env.HOOK_TOKEN);
 
 // Events
 
 // Ready Event is when it connects to discord and is ready to receive events
-dc.once('ready', () => {
+dc.once('ready', async () => {
 	console.log('Ready!');
 	// Send hello to the general channel (The IDS are in config.json)
-//	dc.channels.cache.get(config.generalId).send('Hello');
+	dc.channels.cache.get(config.generalId).send('Hello');
 });
 
 // This event is fired every time a message is sent
-dc.on('message', message => {
-
+dc.on('message', async (message) => {
 	// Look only in the general channel, and ignore the !rank command (This is done later) and if a message starts with the prefix (in config.json)
-	if ((message.channel.id == config.generalId) || (message.channel.id == config.otherChannelId) && (message.content.startsWith(config.prefix))) {
-
+	if (
+		message.channel.id == config.generalId ||
+		(message.channel.id == config.otherChannelId && message.content.startsWith(config.prefix))
+	) {
 		// Removes the prefix from the string so it is easier to deal with
 		const msg = message.content.substr(1);
 
@@ -36,28 +35,24 @@ dc.on('message', message => {
 	}
 });
 
-
 // if a user comes online, the bot will respond 'Hello @user'
 /* This may be disabled because it is very annoying This event fires every time a user's status changes */
-dc.on('presenceUpdate', (oldPresence, newPresence) => {
-
+dc.on('presenceUpdate', async (oldPresence, newPresence) => {
 	// If the user was previously offline and now online, this runs
 	if (newPresence.status == 'online' && oldPresence.status == 'offline') {
-
 		// Finds the user's username and splits it based on # (Discord shows usernames with their 4 digit id such as @someone#1234/ This removes the number)
-		const usr = (newPresence.user.tag).split('#');
+		const usr = newPresence.user.tag.split('#');
 
 		// Sets a welcome message. The split command creates an array, so the first element in the array is the username, and the second is the number, which we do not need
-		const welmsg = (`Welcome ${usr[0]}`);
+		const welmsg = `Welcome ${usr[0]}`;
 
 		// Sends the welcome message in the general channel
 		dc.channels.cache.get(config.generalId).send(welmsg, {
-
 			// Sets the reply attribute so it can mention the user
-			'reply': newPresence.user,
+			reply: newPresence.user,
 		});
 	}
 });
 
 // This logs the server into discord using the token (Everything is events, so it can run last)
-dc.login(token);
+dc.login(process.env.TOKEN);
